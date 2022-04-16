8a3ee475c2bdd4a63b3b21951a90916dd4001722
peltho <boitutile@gmail.com>
2022-04-16 12:50:50 +0200

    Add TailwindCSS + a17t plugin

--- src/_app.tsx
+++ src/_app.tsx
@@ -2,7 +2,14 @@ import React from "react"
 
 const App = () => {
     return (
-        <h1>💖 Hello world!</h1>
+        <div>
+            <h1 className="text-3xl font-bold mb-5">💖 Hello world!</h1>
+            <div className="card">
+                <p className="support">Cinema</p>
+                <h3 className="text-xl heading mb-2">What would you have done differently if..</h3>
+                <p>Jurassic Park was an incredible idea and a magnificent feat of engineering, but poor protocols and a disregard for human safety killed what we could have otherwise been one of the best businesses of our generation.</p>
+            </div>
+        </div>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './_app'
+import "./input.css"
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
